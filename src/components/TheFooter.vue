b19c0cf6da49019c97ebf0602494fd36e001e34a
Inquinima <imanodesu@nodeadline.ru>
2023-09-23 22:17:48 +0600

    Added hover+focus + changed mobile sizes for header and footer links

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,13 +1,21 @@
 <template>
     <footer class="py-6 border-t border-t-primary/40">
         <div class="container px-4">
-            <div class="flex flex-row justify-between items-center">
+            <div class="flex flex-row justify-between items-center max-md:flex-col max-md:text-center max-md:gap-y-4">
                 <div>
                     <p>This tool is created out of need to generate object lists to use in Tailwind config</p>
                 </div>
                 <div>
                     <p>Hope it helps</p>
                 </div>
+            </div>
+            <div class="mt-6">
+                <ul class="flex flex-row justify-between items-center gap-x-4 max-sm:gap-y-4 max-sm:flex-col">
+                    <li><a class="hover:text-accent-main focus:text-accent-main duration-200 transition-colors" target="_blank" href="https://github.com/inquinimaris">GitHub</a></li>
+                    <li><a class="hover:text-accent-main focus:text-accent-main duration-200 transition-colors" target="_blank" href="https://discord.com/users/423965435654963211">Discord</a></li>
+                    <li><a class="hover:text-accent-main focus:text-accent-main duration-200 transition-colors" target="_blank" href="https://t.me/inquinimaris">Telegram</a></li>
+                    <li><a class="hover:text-accent-main focus:text-accent-main duration-200 transition-colors" target="_blank" href="mailto:inquinima@gmail.com">inquinima@gmail.com</a></li>
+                </ul>
             </div>
         </div>
     </footer>
